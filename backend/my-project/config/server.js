5614407fc7ee9f4a43a9790d3ded4367b9e98e5b
guinuto <guiguievangelista18@gmail.com>
2023-11-22 20:01:30 -0300

    revert commit

--- backend/my-project/config/server.js
+++ backend/my-project/config/server.js
@@ -2,12 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array(['r+0wgRgf1bD1tpcKroCC/A==','LkUONnbJIL6l6OMZlOCfBA==','O9MZOMIqsk8Dq/8uxma/Bg==','zpxBTKgwFmHWO7rv2ROzNg==']),
-  },
-  middleware: {
-    app: {
-      keys: ['myKeyA', 'myKeyB'],
-    },
+    keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
