e3e39e7d11954534dc53ce636f23be509dd9e4ff
guinuto <guiguievangelista18@gmail.com>
2023-11-22 19:24:43 -0300

    mudando key

--- backend/my-project/config/server.js
+++ backend/my-project/config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array(['myKeyA']),
+    keys: env.array(['r+0wgRgf1bD1tpcKroCC/A==','LkUONnbJIL6l6OMZlOCfBA==','O9MZOMIqsk8Dq/8uxma/Bg==','zpxBTKgwFmHWO7rv2ROzNg==']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
