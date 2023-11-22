8e4341f02c78aceac072f2942840ab7e49cf2000
guinuto <guiguievangelista18@gmail.com>
2023-11-22 19:47:54 -0300

    arrumando server,js

--- backend/my-project/config/server.js
+++ backend/my-project/config/server.js
@@ -8,6 +8,7 @@ module.exports = ({ env }) => ({
     app: {
       keys: ['myKeyA', 'myKeyB'],
     },
+  },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
