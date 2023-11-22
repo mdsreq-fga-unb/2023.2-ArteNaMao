41edca75d5628fe26d38e391e571043c9101f9c7
guinuto <guiguievangelista18@gmail.com>
2023-11-22 19:44:27 -0300

    add middwer

--- backend/my-project/config/server.js
+++ backend/my-project/config/server.js
@@ -4,6 +4,10 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array(['r+0wgRgf1bD1tpcKroCC/A==','LkUONnbJIL6l6OMZlOCfBA==','O9MZOMIqsk8Dq/8uxma/Bg==','zpxBTKgwFmHWO7rv2ROzNg==']),
   },
+  middleware: {
+    app: {
+      keys: ['myKeyA', 'myKeyB'],
+    },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
